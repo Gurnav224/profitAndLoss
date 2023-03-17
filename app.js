271746bfe4dc1cea90054df023db5cec6fa9d292
Gurnav chaudhary <chaudharyg856@gmail.com>
2023-03-17 13:39:00 +0530

    profit and loss

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log("working");
+const intialPrice = document.querySelector('#intial-price');
+const quantity = document.querySelector('#quantity');
+const currentPrice = document.querySelector('#current-price');
+const showBtn = document.querySelector('#show-btn');
+const outputBox = document.querySelector('#output');
+
+
+
+
+
+
+function calulateProfitAndLoss(intialPrice,quantity,currentPrice){
+
+    if(intialPrice>currentPrice){
+        // loss
+        let loss = (intialPrice-currentPrice)*quantity;
+        let lossPercentage = loss/(intialPrice*quantity)*100;
+
+        showOutput(`Whoops! Your Loss is ${loss} and loss Percentage is ${lossPercentage.toFixed(2)} %`);
+        
+    }
+    else if(currentPrice>intialPrice){
+          // profit
+          let profit = (currentPrice-intialPrice)*quantity;
+          let profitPercentage = profit/(intialPrice*quantity)*100;
+
+          showOutput(`YAY! Your Profit is ${profit} and Profit Percentage is ${profitPercentage.toFixed(2)} %`);
+    }
+    else{
+      // equal
+     showOutput('no pain no gain and no gain no pain');
+    }
+}
+
+function showOutput(message){
+    outputBox.innerHTML = message
+}
+
+
+
+
+function clickHandler(){
+    let ip = Number(intialPrice.value);
+    let qty = Number(quantity.value);
+    let curr = Number(currentPrice.value);
+
+    if(ip && qty && curr){
+       calulateProfitAndLoss(ip,qty,curr)
+    }
+    else{
+      alert("please fill out the all fields")
+    }
+}
+
+
+
+
+showBtn.addEventListener('click',clickHandler);
